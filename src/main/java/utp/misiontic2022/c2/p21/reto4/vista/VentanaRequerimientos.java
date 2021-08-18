3462837e973c55b8dd6e8d949d97129ed30cb4d5
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-08-18 18:37:04 -0500

    Commit

--- src/main/java/utp/misiontic2022/c2/p21/reto4/vista/VentanaRequerimientos.java
+++ src/main/java/utp/misiontic2022/c2/p21/reto4/vista/VentanaRequerimientos.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+//import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
