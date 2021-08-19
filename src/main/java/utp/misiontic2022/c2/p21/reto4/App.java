ca28979e887cc94af4c7c92a2bfda33294f5ed74
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-08-19 18:07:06 -0500

    Console request removed

--- src/main/java/utp/misiontic2022/c2/p21/reto4/App.java
+++ src/main/java/utp/misiontic2022/c2/p21/reto4/App.java
@@ -1,7 +1,7 @@
 package utp.misiontic2022.c2.p21.reto4;
 
 import utp.misiontic2022.c2.p21.reto4.vista.VentanaRequerimientos;
-import utp.misiontic2022.c2.p21.reto4.vista.VistaRequerimientos;
+//import utp.misiontic2022.c2.p21.reto4.vista.VistaRequerimientos;
 
 /**
  * Esta clase solo se debe usar para hacer pruebas locales,
